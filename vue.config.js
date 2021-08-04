08c3b90f0b632dec79453088c608afcb51a763b1
Donovan <donovant78@gmail.com>
2021-08-05 00:27:20 +0200

    Add buildResources folder and logos

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,22 @@ module.exports = {
 	pluginOptions: {
 		electronBuilder: {
 			preload: { ipcPcc: "src/preload/ipcPcc.js", ipcFilter: "src/preload/ipcFilter.js" },
+			builderOptions: {
+				appId: "com.Unlighter.unlighter",
+				productName: "Unlighter",
+				win: {
+					target: [ "nsis" ]
+				},
+				nsis: {
+					artifactName: "Unlighter-Setup-${version}.${ext}",
+					createDesktopShortcut: "always",
+					createStartMenuShortcut: true,
+					shortcutName: "Unlighter"
+				},
+				directories: {
+				  buildResources: "resources"
+				},
+			}
 		},
 	},
 	configureWebpack: {
