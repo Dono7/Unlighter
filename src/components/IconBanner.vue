80addd7ea70e36d9a5535eef1a45b566d4a76bec
Donovan <donovant78@gmail.com>
2021-11-19 21:10:06 +0100

    Add Icon Banner

---
+++ src/components/IconBanner.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+	<div class="icon-banner">
+		<img src="@/assets/svg/icon-colored.svg" alt="Unlighter Icon Logo Banner" />
+		<p class="icon-banner-title">Unlighter</p>
+	</div>
+</template>
+
+<style lang="sass" scoped>
+.icon-banner
+	display: flex
+	align-items: center
+	padding: 0 28px
+	height: 72px
+	img
+		height: 32px
+		margin-right: 12px
+	&-title
+		font-size: 14px
+</style>
--- src/views/Pcc.vue
+++ src/views/Pcc.vue
@@ -1,8 +1,12 @@
 <script setup>
+import TitleBar from "../components/TitleBar.vue"
+import IconBanner from "../components/IconBanner.vue"
 import MonitorsController from "../components/MonitorsController.vue"
 </script>
 
 <template>
+	<TitleBar />
+	<IconBanner />
 	<div class="monitors">
 		<MonitorsController />
 	</div>
