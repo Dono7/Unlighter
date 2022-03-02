ee5c68f3f12ab84fb0f6f12f78ca262538d3d2ed
Donovan <donovant78@gmail.com>
2022-03-02 12:58:46 -0500

    Add showPerf field to Config

--- src/modules/Perf.js
+++ src/modules/Perf.js
@@ -1,6 +1,10 @@
+import config from "./../config.json"
+
 const perfStart = performance.now()
 
-const perfDisplay = (text) =>
-	console.log(`${Math.round(performance.now() - perfStart)}ms : ${text}`)
+const perfDisplay = (text) => {
+	if (config.showPerf)
+		console.log(`${Math.round(performance.now() - perfStart)}ms : ${text}`)
+}
 
 export { perfDisplay }
